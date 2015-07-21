c30f55bb1638a58674ccbec009ee3798a93cfd58
James Henderson <james@jarohen.me.uk>
2015-07-21 13:29:34 +0100

    Clojure version bump + SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject jarohen/embed-nrepl "0.1.1"
+(defproject jarohen/embed-nrepl "0.1.2-SNAPSHOT"
   :description "A micro-library to start up an nREPL server with my opinionated defaults"
   :url "https://github.com/james-henderson/embed-nrepl"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
 
                  [org.clojure/tools.nrepl "0.2.10"]
                  [cider/cider-nrepl "0.9.1"]
